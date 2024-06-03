721ee6d8c55d1951ce3ef451136e460b34d7c840
Harshit2973 <harshit3789.be21@chitkara.edu.in>
2024-06-03 10:13:59 +0530

    added logout

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 from .serializers import SignUpSerializer, EmailLoginSerializer
 
 class SignupView(APIView):
@@ -28,3 +30,14 @@ class LoginView(APIView):
             return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
         print(serializer.errors)  # Debugging: print serializer errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            token = Token.objects.get(user=request.user)
+            token.delete()
+            return Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"message": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
